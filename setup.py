6c340df3fdcbf2ae30d2527ef87619dca0f93b62
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-02 17:15:05 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    * use centralized publish workflow
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2015, 2016 CERN.
-# SPDX-FileCopyrightText: 2024, California Institute of Technology.
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""Python API wrapper for the DataCite Metadata Store and REST APIs."""
-
-from setuptools import setup
-
-setup()
